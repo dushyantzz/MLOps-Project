bfad7cd3f7b30d0b8cab145540bcea878d7db066
dushyantzz <157586156+dushyantzz@users.noreply.github.com>
2024-12-30 14:47:47 +0530

    making pipeline by artifacts

--- demo.py
+++ demo.py
@@ -1,7 +1,13 @@
-from src.logger import logging
+# from src.logger import logging
 
-logging.debug("This is a debug message.")
-logging.info("This is an info message.")
-logging.warning("This is a warning message.")
-logging.error("This is an error message.")
-logging.critical("This is a critical message.")
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
+
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
